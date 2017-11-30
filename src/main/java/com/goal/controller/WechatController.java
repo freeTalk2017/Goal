061b595b8b0162bd49dbf3e50b61daeb75863465
mcgeewang <vndpfwan@cn.ibm.com>
2017-11-30 19:49:54 +0800

    test

--- src/main/java/com/goal/controller/WechatController.java
+++ src/main/java/com/goal/controller/WechatController.java
@@ -26,54 +26,71 @@ public class WechatController extends AbstractController{
 	@Resource
 	WechatControllerHelper wechatControllerHelper;
 	
-	@RequestMapping("/code")
-	public @ResponseBody Object getCode(){
-		logger.debug("do get request code");
-		final Boolean isCode = true;
-		RequestUtil requestUtil = new RequestUtil();
-		String url = WXRequestUtil.getSilentUrl();
-		if(url != null){
-			try {
-				requestUtil.doGetUrl(url,isCode);
-			} catch (Exception e) {
-				logger.error("do get request code error");
-			}
-		}else{
-			logger.debug("url is null !");
-		}
-		return null;
-	}
+//	@RequestMapping("/code")
+//	public @ResponseBody Object getCode(){
+//		logger.debug("do get request code");
+//		final Boolean isCode = true;
+//		RequestUtil requestUtil = new RequestUtil();
+//		String url = WXRequestUtil.getSilentUrl();
+//		if(url != null){
+//			try {
+//				requestUtil.doGetUrl(url,isCode);
+//			} catch (Exception e) {
+//				logger.error("do get request code error");
+//			}
+//		}else{
+//			logger.debug("url is null !");
+//		}
+//		return null;
+//	}
 	
-	@RequestMapping("/check")
+	@RequestMapping("/code")
 	public @ResponseBody Object callbackBase(@RequestParam(value = "code", required = false) String code, HttpServletResponse response){
-		logger.debug("do callback base");
-		
-		Map<String, String> result = null;
-		PrepayForm prepayForm = null;
-		
-		String openid = wechatControllerHelper.getOpenIdBySlientAuthy(code);
-		logger.debug("openid = {}",openid);
-		
-		if("".equals(openid)){
-			logger.debug("openid is null");
+		if (code == null){
+			logger.debug("do get request code");
+			final Boolean isCode = true;
+			RequestUtil requestUtil = new RequestUtil();
+			String url = WXRequestUtil.getSilentUrl();
+			if(url != null){
+				try {
+					requestUtil.doGetUrl(url,isCode);
+				} catch (Exception e) {
+					logger.error("do get request code error");
+				}
+			}else{
+				logger.debug("url is null !");
+			}
 			return null;
+		}else{
+			logger.debug("do callback base");
+			
+			Map<String, String> result = null;
+			PrepayForm prepayForm = null;
+			
+			String openid = wechatControllerHelper.getOpenIdBySlientAuthy(code);
+			logger.debug("openid = {}",openid);
+			
+			if("".equals(openid)){
+				logger.debug("openid is null");
+				return null;
+			}
+			
+			result = wechatControllerHelper.doUnifiedOrder(openid);
+			
+			if("".equals(result)){
+				logger.debug("unified order is null");
+				return null;
+			}
+			
+			prepayForm =wechatControllerHelper.generatePrepayForm(result);
+			
+			if("".equals(prepayForm)){
+				logger.debug("prepay form is null");
+				return null;
+			}
+			
+			return prepayForm;
 		}
-		
-		result = wechatControllerHelper.doUnifiedOrder(openid);
-		
-		if("".equals(result)){
-			logger.debug("unified order is null");
-			return null;
-		}
-		
-		prepayForm =wechatControllerHelper.generatePrepayForm(result);
-		
-		if("".equals(prepayForm)){
-			logger.debug("prepay form is null");
-			return null;
-		}
-		
-		return prepayForm;
 	}
 	
 }
